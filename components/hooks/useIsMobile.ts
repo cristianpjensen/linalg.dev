cf20f26c4a8556fb24e5990cfb5a0063ce62eb78
Cristian Perez Jensen <cristianpjensen@gmail.com>
2022-08-09 18:39:45 +0200

    fix: mobile state not updating immediately

--- components/hooks/useIsMobile.ts
+++ components/hooks/useIsMobile.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const useIsMobile = () => {
-	const [isMobile, setIsMobile] = useState(false);
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
 
 	useEffect(() => {
 		const handleResize = () => {
