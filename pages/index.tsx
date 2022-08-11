30c82f0583ecc67036b91168c441230573918e8d
Cristian Perez Jensen <cristianpjensen@gmail.com>
2022-08-11 14:51:08 +0200

    fix: overlaying divs making the background disappear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import { useWindowHeight } from "@react-hook/window-size";
 
 const App = dynamic(() => import("../src/App"), {
 	ssr: false,
 });
 
 const Home: NextPage = () => {
+	const height = useWindowHeight();
+
 	return (
-		<div>
+		<div className="absolute top-0 left-0 w-full h-full" style={{ height }}>
 			<Head>
 				<title>Linear algebra node environment</title>
 				<meta
